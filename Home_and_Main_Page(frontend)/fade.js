d47170dae2c277a1fa8b5603676668d7967c2c8e
Samridhi Sinha <162716314+Samridhi024@users.noreply.github.com>
2025-06-17 02:11:45 +0530

    Delete Home_and_Main_Page(frontend) directory

--- Home_and_Main_Page(frontend)/fade.js
+++
@@ -1,9 +0,0 @@
-import Highway from "@dogstudio/highway";
-import Fade from "C:\Dust Collector/fade.js" ;
-import { gsap } from "gsap"; 
-
-const H = new Highway.Core({
-    transitions: {
-        default: Fade  
-    }
-});
