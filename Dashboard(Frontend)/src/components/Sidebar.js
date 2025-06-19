31a8c41379afeba0cdd3faafa9be7aa81557ea68
Samridhi Sinha <162716314+Samridhi024@users.noreply.github.com>
2025-06-19 18:22:20 +0530

    Update Sidebar.js

--- Dashboard(Frontend)/src/components/Sidebar.js
+++ Dashboard(Frontend)/src/components/Sidebar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import dashboardIcon from "./dashb.png";
+import tipsIcon from "./ht.png";
+import contactIcon from "./cont.png";
 
 function Sidebar() {
   const username = localStorage.getItem("username") || "Anonymous User";
@@ -14,17 +17,28 @@ function Sidebar() {
   return (
     <div
       className="text-white d-flex flex-column justify-content-between ps-2"
-      style={{ width: "250px", minHeight: "100vh", backgroundColor: 'rgba(60, 57, 57, 0.93)' }}
+      style={{ width: "250px", minHeight: "100vh", backgroundColor: "#1B252B" }}
     >
       <div>
-        <h4 className="my-4 mx-2 mb-4">DUST</h4>
-        <h4 className="my-4 mx-2 mb-2">MONITOR</h4>
+        <h2 className="my-4 mx-2 mb-4">DUST</h2>
+        <h2 className="my-4 mx-2 mb-2">MONITOR</h2>
         <Nav defaultActiveKey="/dashboard" className="flex-column">
-          <hr className="bg-light" />
-          <Nav.Link as={Link} to="/" className="text-white">Dashboard</Nav.Link>
-          <Nav.Link as={Link} to="/health-tips" className="text-white">Health Tips</Nav.Link>
-          <Nav.Link as={Link} to="/contact" className="text-white">Contact</Nav.Link> 
+          <Nav.Link as={Link} to="/" className="text-white d-flex align-items-center mb-3">
+            <img src={dashboardIcon} alt="Dashboard" style={{ width: "24px", marginRight: "10px" }} />
+            Dashboard
+          </Nav.Link>
+
+          <Nav.Link as={Link} to="/health-tips" className="text-white d-flex align-items-center mb-3">
+            <img src={tipsIcon} alt="Health Tips" style={{ width: "24px", marginRight: "10px" }} />
+            Health Tips
+          </Nav.Link>
+
+          <Nav.Link as={Link} to="/contact" className="text-white d-flex align-items-center mb-3">
+            <img src={contactIcon} alt="Contact" style={{ width: "24px", marginRight: "10px" }} />
+            Contact
+          </Nav.Link>
         </Nav>
+
       </div>
 
       <div className="p-2">
@@ -43,6 +57,11 @@ function Sidebar() {
         <Nav.Link onClick={handleLogout} className="text-white mt-2">Sign Out</Nav.Link>
       </div>
     </div>
+  );
+}
+
+export default Sidebar;
+
   );
 }
 
